f1f589dd279213a93d44f4e7590eccc8c7fc5c46
Guillermo575 <guille_medina575@hotmail.com>
2021-04-14 00:24:35 -0500

    Menu changes

---
+++ Project/SpinnerRocket/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject MenuStart;
+    public GameObject MenuOptions;
+    [Header("Levels To Load")]
+    public string _newGameButtonLevel;
+    void Start()
+    {       
+    }
+    void Update()
+    {       
+    }
+    public void ClickNewGameDialog()
+    {
+        SceneManager.LoadScene(_newGameButtonLevel);
+    }
+    public void ClickExitGame()
+    {
+        Application.Quit();
+    }
+    public void ClickShowOptions()
+    {
+        HideCanvas();
+        MenuOptions.SetActive(true);
+    }
+    public void ClickShowStart()
+    {
+        HideCanvas();
+        MenuStart.SetActive(true);
+    }
+    public void HideCanvas()
+    {
+        MenuStart.SetActive(false);
+        MenuOptions.SetActive(false);
+    }
+}
