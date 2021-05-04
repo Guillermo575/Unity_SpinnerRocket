861decc6d1fc005019ec4e98e4c51325e539e5cd
Guillermo575 <guille_medina575@hotmail.com>
2021-05-03 23:54:52 -0500

    Math RNG

--- Project/SpinnerRocket/Assets/Scripts/MathRNG.cs
+++ Project/SpinnerRocket/Assets/Scripts/MathRNG.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using UnityEngine;
 public class MathRNG
 {
+    #region Variables
     public decimal MinValue { get; set; }
     public decimal MaxValue { get; set; }
     public int Seed { get; set; }
     private int Next { get; set; }
+    #endregion
+
+    #region Constructors
     public MathRNG(decimal MinValue, decimal MaxValue, int Seed = 1)
     {
         this.MinValue = MinValue;
@@ -15,7 +19,18 @@ public class MathRNG
         this.Seed = Seed;
         Next = 1;
     }
+    public MathRNG(int Seed = 1)
+    {
+        this.Seed = Seed;
+    }
+    #endregion
+
+    #region NextValues
     public decimal NextValue()
+    {
+        return NextValue(MinValue, MaxValue);
+    }
+    public decimal NextValue(decimal MinValue, decimal MaxValue)
     {
         decimal SeedModify = Seed;
         SeedModify = Next % 3 == 0 ? (decimal)(Seed * 0.0001) : SeedModify;
@@ -27,17 +42,35 @@ public class MathRNG
         Next++;
         return PseudoRandom;
     }
+    #endregion
+
+    #region Test Area
     public static void Testing()
     {
-        MathRNG obj = new MathRNG(1, 100, 456785763);
-        var lst = new List<String>();
-        for (int l = 0; l < 100; l++)
+        int ciclos = 100;
+        int SumatoriaProm = 0;
+        int Maximo = 0;
+        int Minimo = 0;
+        for (int m = 0; m < ciclos; m++)
         {
-            lst.Add(((Int32)obj.NextValue()).ToString());
+            MathRNG obj = new MathRNG(1, 100, UnityEngine.Random.Range(1, 999999999));
+            var lst = new List<String>();
+            for (int l = 0; l < 300; l++)
+            {
+                lst.Add(((Int32)obj.NextValue()).ToString());
+            }
+            var lstgr = lst.Distinct();
+            SumatoriaProm += lstgr.Count();
+            Maximo = lstgr.Count() > Maximo ? lstgr.Count() : Maximo;
+            Minimo = lstgr.Count() < Minimo || Minimo == 0 ? lstgr.Count() : Minimo;
+            //Debug.Log("Seed " + obj.Seed + ": " + lstgr.Count());
+            //Debug.Log(String.Join(", ", lst));
+            //Debug.Log(String.Join(", ", lstgr));
         }
-        var lstgr = lst.Distinct();
-        Debug.Log(lstgr.Count());
-        //Debug.Log(String.Join(", ", lst));
-        //Debug.Log(String.Join(", ", lstgr));
+        SumatoriaProm = SumatoriaProm / ciclos;
+        Debug.Log("Maximo: " + Maximo);
+        Debug.Log("Promedio: " + SumatoriaProm);
+        Debug.Log("Minimo: " + Minimo);
     }
+    #endregion
 }
