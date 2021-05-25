879c4ddf77322438688e7c06d32cdcddee8e3a43
Guillermo575 <guille_medina575@hotmail.com>
2021-05-24 21:31:10 -0500

    Camer begavior changes

--- Project/SpinnerRocket/Assets/Scripts/GameBehaviors/GameCameraBehavior.cs
+++ Project/SpinnerRocket/Assets/Scripts/GameBehaviors/GameCameraBehavior.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class GameCameraBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [HideInInspector] public GameManager objGameManager;
+    public Camera Camera;
+    public Transform target;
+    public Vector3 offset;
+    [Range(0, 10)] public float smoothFactor;
     void Start()
     {
-        
+        objGameManager = GameObject.Find("GameManager").gameObject.GetComponentsInChildren<GameManager>(true)[0];
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        FollowCamera();
+    }
+    public void FollowCamera()
+    {
+        float height = 2f * Camera.orthographicSize;
+        float width = height * Camera.aspect;
+        Vector3 targetPosition = target.position + offset;
+        Vector3 boundPosition = new Vector3(Mathf.Clamp(targetPosition.x, objGameManager.minValues.x + (width / 2), objGameManager.maxValues.x - (width / 2)), Mathf.Clamp(targetPosition.y, objGameManager.minValues.y + (height / 2), objGameManager.maxValues.y - (height / 2)), -10);
+        Vector3 smoothPosition = Vector3.Lerp(Camera.transform.position, boundPosition, smoothFactor * Time.fixedDeltaTime);
+        Camera.transform.position = (smoothFactor == 0) ? boundPosition : smoothPosition;
     }
 }
