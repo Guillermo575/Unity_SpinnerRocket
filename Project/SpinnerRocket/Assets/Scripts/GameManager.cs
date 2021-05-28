95f70e22543d589626b0ca19aedd18f1a4327136
Guillermo575 <guille_medina575@hotmail.com>
2021-05-27 23:46:29 -0500

    TileGrid Boundaries

--- Project/SpinnerRocket/Assets/Scripts/GameManager.cs
+++ Project/SpinnerRocket/Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
 public class GameManager : MonoBehaviour
 {
     #region Variables
@@ -7,20 +9,24 @@ public class GameManager : MonoBehaviour
     [HideInInspector] public bool GameOver = false;
     [HideInInspector] public int Score = 0;
     [HideInInspector] public MathRNG objMathRNG = new MathRNG(45289574);
-
     [Header("Static")]
     [HideInInspector] public static bool PauseGame = false;
     [HideInInspector] public static bool MuteGame;
     [HideInInspector] public static bool BlockKeyBoard = false;
-
     [Header("Scene Bounds")]
-    public Vector3 minValues;
-    public Vector3 maxValues;
+    [HideInInspector] public Vector3 minValues;
+    [HideInInspector] public Vector3 maxValues;
+    public Tilemap TileGrid;
     #endregion
 
     #region Start & Update
     void Start()
     {
+        if(TileGrid != null)
+        {
+            minValues = new Vector3(TileGrid.editorPreviewOrigin.x, TileGrid.editorPreviewOrigin.y, 0);
+            maxValues = new Vector3(TileGrid.editorPreviewOrigin.x + TileGrid.editorPreviewSize.x, TileGrid.editorPreviewOrigin.y + TileGrid.editorPreviewSize.y, 0);
+        }
         StartGame = true;
         PauseGame = false;
         GameOver = false;
@@ -29,13 +35,13 @@ public class GameManager : MonoBehaviour
         var lstObjects = this.gameObject.GetComponentsInChildren<SpawnObject>(true);
         foreach (var obj in lstObjects)
         {
+            obj.TileGrid = obj.TileGrid == null ? TileGrid : obj.TileGrid;
             obj.objMathRNG = objMathRNG;
             obj.Spawn();
         }
     }
     void Update()
     {
-
     }
     #endregion
 }
--- Project/SpinnerRocket/Assets/Scripts/GameObjects/SpawnObject.cs
+++ Project/SpinnerRocket/Assets/Scripts/GameObjects/SpawnObject.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Tilemaps;
 public class SpawnObject : MonoBehaviour
 {
     #region Hidden
@@ -19,10 +18,11 @@ public class SpawnObject : MonoBehaviour
         InScene = 2,
     }
     public SpawnType type;
-
     [Header("Scene Bounds")]
-    public Vector3 minValues;
-    public Vector3 maxValues;
+    [HideInInspector] public Vector3 minValues;
+    [HideInInspector] public Vector3 maxValues;
+    public Tilemap TileGrid;
+    public float OffSetUnits;
     #endregion
 
     #region Methods
@@ -31,13 +31,20 @@ public class SpawnObject : MonoBehaviour
     }
     public void Spawn()
     {
+        if (TileGrid != null)
+        {
+            minValues = new Vector3(TileGrid.editorPreviewOrigin.x, TileGrid.editorPreviewOrigin.y, 0);
+            maxValues = new Vector3(TileGrid.editorPreviewOrigin.x + TileGrid.editorPreviewSize.x, TileGrid.editorPreviewOrigin.y + TileGrid.editorPreviewSize.y, 0);
+        }
+        var min = new Vector2(minValues.x + OffSetUnits, minValues.y + OffSetUnits);
+        var max = new Vector2(maxValues.x - OffSetUnits, maxValues.y - OffSetUnits);
         for (int i = 0; i < Quantity; i++)
         {
             Vector2 position = new Vector2();
             switch(type)
             {
-                case SpawnType.Border: position = objMathRNG.getRandomSpawnPoint(minValues, maxValues); break;
-                case SpawnType.InScene: position = new Vector2(objMathRNG.NextValueFloat(minValues.x, maxValues.x), objMathRNG.NextValueFloat(minValues.y, maxValues.y)); break;
+                case SpawnType.Border: position = objMathRNG.getRandomSpawnPoint(min, max); break;
+                case SpawnType.InScene: position = new Vector2(objMathRNG.NextValueFloat(min.x, max.x), objMathRNG.NextValueFloat(min.y, max.y)); break;
             }
             lstObj.Add(Instantiate(obj, position, Quaternion.identity));
         }
