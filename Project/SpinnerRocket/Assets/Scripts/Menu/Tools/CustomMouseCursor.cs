1df425ec7589331fd4f110c67f01bcc6f3486dce
Guillermo575 <guille_medina575@hotmail.com>
2021-06-03 20:30:41 -0500

    Mouse cursor

---
+++ Project/SpinnerRocket/Assets/Scripts/Menu/Tools/CustomMouseCursor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CustomMouseCursor : MonoBehaviour
+{
+    [HideInInspector] Vector2 hotSpot = new Vector2(0, 0);
+    [HideInInspector] CursorMode cursorMode = CursorMode.Auto;
+    public Texture2D CursorNormal;
+    public Texture2D CursorPress;
+    void Start()
+    {
+        Cursor.SetCursor(CursorNormal, hotSpot, cursorMode);
+    }
+    public void OnEnterExit()
+    {
+        Cursor.SetCursor(CursorNormal, hotSpot, cursorMode);
+    }
+    public void OnEnterPress()
+    {
+        Cursor.SetCursor(CursorPress, hotSpot, cursorMode);
+    }
+}
